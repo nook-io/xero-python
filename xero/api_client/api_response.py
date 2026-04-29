2eada50974580123a26aff34cc46f2d8a66e9ec9
Johnny Deuss <6266815+JohnnyDeuss@users.noreply.github.com>
2026-04-29 13:01:12 +0100

    Remove Pydantic annotations on non-Pydantic class ApiResponse

--- xero/api_client/api_response.py
+++ xero/api_client/api_response.py
@@ -2,14 +2,12 @@ from __future__ import annotations
 
 from typing import Any
 
-from pydantic import Field, StrictInt, StrictStr
-
 
 class ApiResponse:
-    status_code: StrictInt | None = Field(None, description="HTTP status code")
-    headers: dict[StrictStr, StrictStr] | None = Field(None, description="HTTP headers")
-    data: Any | None = Field(None, description="Deserialized data given the data type")
-    raw_data: Any | None = Field(None, description="Raw data (HTTP response body)")
+    status_code: int | None = None  # "HTTP status code"
+    headers: dict[str, str] | None = None  # HTTP headers
+    data: Any | None = None  # Deserialized data given the data type
+    raw_data: Any | None = None  # Raw data (HTTP response body)
 
     def __init__(self, status_code=None, headers=None, data=None, raw_data=None) -> None:
         self.status_code = status_code
